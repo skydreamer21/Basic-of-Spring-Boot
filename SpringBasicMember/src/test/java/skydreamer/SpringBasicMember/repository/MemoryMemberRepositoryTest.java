4f83cac4e76897b0ec5e0813dd1c5326956e1ace
skydreamer21 <skydreamer210@gmail.com>
2022-10-25 23:19:41 +0900

    [Test] MemberRepository Unit Test (Lecture 11)

---
+++ SpringBasicMember/src/test/java/skydreamer/SpringBasicMember/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,63 @@
+package skydreamer.SpringBasicMember.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+import skydreamer.SpringBasicMember.domain.Member;
+
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+// junit에도 Assertions 가 있는데 assertThat을 쓰려면 따로 import 필요
+
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();;
+    }
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+        Member result = repository.findById(member.getId()).get();
+
+//        assertEquals(member, result);
+        assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName(member1.getName()).get();
+        assertThat(member1).isEqualTo(result);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> all = repository.findAll();
+        assertThat(2).isEqualTo(all.size());
+    }
+}
